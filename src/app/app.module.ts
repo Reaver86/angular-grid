114e8c550a9c0959d0445c2cc55449a9f47d7e6b
Alexander Schuster <alexander.schuster@web.de>
2018-10-17 00:06:12 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GridModule } from '@angular/flex-layout/grid';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
